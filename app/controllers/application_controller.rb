4f19f4a68acf48c9d3aacdab0d3c1b63db4b6b10
jordanswp <sohweepin@gmail.com>
2020-06-10 23:50:20 +0800

    added nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,3 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
-class ApplicationController < ActionController::Base
-end
