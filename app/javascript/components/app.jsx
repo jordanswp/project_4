9f79bcf7cc753f0ce647a175df1a37f993c3bd70
jordanswp <sohweepin@gmail.com>
2020-06-11 12:14:48 +0800

    added axios and app.jsx file

---
+++ app/javascript/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import axios from 'axios';
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+
+        };
+    }
+
+  render() {
+    return (
+      <p>test</p>
+    );
+  }
+}
+
+export default App;
